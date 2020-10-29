d7b86daca8d88b82e91fc8876c4f270b02149b5f
Barış Köklü <30669414+bariskoklu@users.noreply.github.com>
2020-10-29 16:47:57 +0300

    dışarıya veya en uçtaki tilelara tıklandığında yaşanan seçme sorunu ortadan kaldırıldı. Şuan oyun bugfree gibi duruyor.

--- HexagonBarisKoklu/Assets/Scripts/SelectTiles.cs
+++ HexagonBarisKoklu/Assets/Scripts/SelectTiles.cs
@@ -34,38 +34,65 @@ public class SelectTiles : MonoBehaviour
     {
         Vector3 mouseposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         TileClass furthestTile = new TileClass(Color.white, 0, 0);
-        //Bütün satır ve sütunları dönüyoruz
         selectedTiles.tileList.Clear();
-        for (int k = 0; k < numberOfRows.value; k++)
+        TileClass mousepositionTile = this.GetTileOnThePosition(mouseposition);
+
+        if (mousepositionTile != null)
         {
-            for (int i = 0; i < numberOfColumns.value; i++)
+            Vector3Int mousepositionTileVector = new Vector3Int(mousepositionTile.x, mousepositionTile.y, 0);
+            //seçilen tile'ın uç tilelarda olması durumunda yanlış seçimi önlemek için yazılmıştır.
+            //Eğer en üstteyse mousepozisyonunun y sini cell'in ortasındaki y pozisyonundan 1 pixel aşağı sabitleyeceğiz. En alttaysa y'sini 1 pixel yukarı sabitleyeceğiz.
+            //En sağda ve solda durumları için de x pozisyonun sırasıyla ortadan bir sol ve bir sağ olarak sabitleyeceğiz.
+            if (mousepositionTile.y == numberOfColumns.value -1)
             {
-                TileClass currentTile = allTiles.getTile(k,i);
-                Vector3Int currentTilePosition = new Vector3Int(k, i, 1);
-                if (currentTile != null)
+                mouseposition.x = tilemap.GetCellCenterWorld(mousepositionTileVector).x - 0.1f;
+            }
+            else if (mousepositionTile.y == 0)
+            {
+                mouseposition.x = tilemap.GetCellCenterWorld(mousepositionTileVector).x + 0.1f;
+            }
+            if (mousepositionTile.x == numberOfRows.value - 1)
+            {
+                mouseposition.y = tilemap.GetCellCenterWorld(mousepositionTileVector).y - 0.1f;
+            }
+            else if (mousepositionTile.x == 0)
+            {
+                mouseposition.y = tilemap.GetCellCenterWorld(mousepositionTileVector).y + 0.1f;
+            }
+
+
+            //Bütün satır ve sütunları dönüyoruz
+            for (int k = 0; k < numberOfRows.value; k++)
+            {
+                for (int i = 0; i < numberOfColumns.value; i++)
                 {
-                    if (selectedTiles.tileList.Count < 3)
+                    TileClass currentTile = allTiles.getTile(k, i);
+                    Vector3Int currentTilePosition = new Vector3Int(k, i, 1);
+                    if (currentTile != null)
                     {
-                        selectedTiles.tileList.Add(currentTile);
-                    }
-                    else
-                    {
-                        furthestTile = currentTile;
-
-                        for (int x = 0; x < selectedTiles.tileList.Count; x++)
+                        if (selectedTiles.tileList.Count < 3)
                         {
-                            float distanceBetweenMouseAndFurthestTile = 0;
-                            distanceBetweenMouseAndFurthestTile = CalculateDistanceBetweenTileAndWorldPos(furthestTile, mouseposition);
-                            TileClass tileToReplace = selectedTiles.tileList[x];
-                            float distanceBetweenMouseAndTileToReplace = CalculateDistanceBetweenTileAndWorldPos(tileToReplace, mouseposition); ;
+                            selectedTiles.tileList.Add(currentTile);
+                        }
+                        else
+                        {
+                            furthestTile = currentTile;
 
-                            if (distanceBetweenMouseAndFurthestTile <= distanceBetweenMouseAndTileToReplace)
+                            for (int x = 0; x < selectedTiles.tileList.Count; x++)
                             {
-                                TileClass tempTile = selectedTiles.tileList[x];
-                                selectedTiles.tileList.RemoveAt(x);
-                                selectedTiles.tileList.Add(furthestTile);
-                                furthestTile = tempTile;
-                                x = 0; //for resetting the loop
+                                float distanceBetweenMouseAndFurthestTile = 0;
+                                distanceBetweenMouseAndFurthestTile = CalculateDistanceBetweenTileAndWorldPos(furthestTile, mouseposition);
+                                TileClass tileToReplace = selectedTiles.tileList[x];
+                                float distanceBetweenMouseAndTileToReplace = CalculateDistanceBetweenTileAndWorldPos(tileToReplace, mouseposition); ;
+
+                                if (distanceBetweenMouseAndFurthestTile <= distanceBetweenMouseAndTileToReplace)
+                                {
+                                    TileClass tempTile = selectedTiles.tileList[x];
+                                    selectedTiles.tileList.RemoveAt(x);
+                                    selectedTiles.tileList.Add(furthestTile);
+                                    furthestTile = tempTile;
+                                    x = 0; //for resetting the loop
+                                }
                             }
                         }
                     }
@@ -90,5 +117,20 @@ public class SelectTiles : MonoBehaviour
         distance = Vector2.Distance(tileWorldSpacePosition, worldPos);
 
         return distance;
+    }
+
+    private TileClass GetTileOnThePosition(Vector3 position)
+    {
+
+        Vector3Int cellPosition = tilemap.WorldToCell(position);
+
+        if (allTiles.tileList.Exists(tileToFind => tileToFind.x == cellPosition.x && tileToFind.y == cellPosition.y))
+        {
+            return allTiles.getTile(cellPosition.x, cellPosition.y);
+        }
+        else
+        {
+            return null;
+        }
     }
 }
